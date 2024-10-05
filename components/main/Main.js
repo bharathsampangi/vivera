7c918fba8682099dd143e28bee3489e336b5f822
bharathsampangi <bharathsampangi95@gmail.com>
2024-10-05 14:34:09 +0530

    remove video

--- components/main/Main.js
+++ components/main/Main.js
@@ -32,7 +32,6 @@ const Main = () => {
                 setIsOpen
             }} />
             <About />
-            <Video />
             <Project {...{
                 openModal
             }}/>
